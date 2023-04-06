db893c206108209dce75648552bdcbd460d18ff7
ᑢᗩᘿᒪᘿSᖶᘿ <studi.jasha.gruener@gmail.com>
2023-04-07 00:10:55 +0200

    added multiple event functions

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import discord
 
 intents = discord.Intents.default()
-intents.typing = False
+intents.typing = True
 intents.presences = False
 intents.message_content = True
+intents.reactions = True
 
 class MyClient(discord.Client):
     #login
@@ -15,11 +16,22 @@ class MyClient(discord.Client):
         # don't react to yourself, bot!
         if message.author == client.user:
             return
-        
         print(str(message.author) + ": " + str(message.content))
         
-        if message.content.startswith("Hello Phantom!"):
+        if message.content.startswith("$hello"):
             await message.channel.send("Hello " + str(message.author)[:-5])
+    
+    #on_typing
+    async def on_typing(self, channel, user, when):
+        return
+    
+    #on_message_deleted
+    async def on_message_delete(self, message):
+        return
+    
+    #on_message_edit
+    async def on_message_edit(self, before, after):
+        return
 
 
 with open("token", "r") as tkn:
