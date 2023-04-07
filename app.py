691d61d0cbce776567f1d8209c635d193e4b7e6b
celeste-42bit <studi.jasha.gruener@gmail.com>
2023-04-07 23:22:05 +0200

    updated log definition

--- app.py
+++ app.py
@@ -5,32 +5,31 @@ intents.typing = True
 intents.presences = False
 intents.message_content = True
 intents.reactions = True
-
 class MyClient(discord.Client):
     #login
     async def on_ready(self):
         print("Login complete!")
-    
+
     #on_message
     async def on_message(self, message):
         # don't react to yourself, bot!
         if message.author == client.user:
             return
         print(str(message.author) + ": " + str(message.content))
-        
+
         if message.content.startswith("$hello"):
             await message.channel.send("Hello " + str(message.author)[:-5])
         if message.content.startswith("$test"):
             await message.channel.send("This is a test.")
-    
+
     #on_typing
     async def on_typing(self, channel, user, when):
         return
-    
+
     #on_message_deleted
     async def on_message_delete(self, message):
         return
-    
+
     #on_message_edit
     async def on_message_edit(self, before, after):
         return
