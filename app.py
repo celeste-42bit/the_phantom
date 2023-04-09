89c4171114d17dd3839df1ac05b06d22bd8d2b1b
celeste-42bit <studi.jasha.gruener@gmail.com>
2023-04-09 14:05:27 +0200

    corrected some grammar, spelling and formatting of 'app.py'

--- app.py
+++ app.py
@@ -5,11 +5,16 @@ intents.typing = True
 intents.presences = False
 intents.message_content = True
 intents.reactions = True
+
+
 class MyClient(discord.Client):
+
+
     #login
     async def on_ready(self):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
         print('----------------------------------------------')
+
 
     #on_message
     async def on_message(self, message):
@@ -23,19 +28,21 @@ class MyClient(discord.Client):
         await welcome_channel.send("Woo!")
         '''
         print(str(message.channel.id))
-
         print(str(message.author) + ": " + str(message.content))
 
         if message.content.startswith("$hello"):
             await message.channel.send("Hello " + str(message.author)[:-5])  # [:-5] cut last 5 symbols (#0000)
 
+
     #on_typing
     async def on_typing(self, channel, user, when):
         return
 
+
     #on_message_deleted
     async def on_message_delete(self, message):
         return
+
 
     '''
     #on_message_edit
@@ -49,25 +56,33 @@ class MyClient(discord.Client):
         #return
     '''
 
+
     async def on_reaction_remove(self, reaction, user):
         return
+
 
     async def on_raw_reaction_add(self, payload):
         channel = client.get_channel(payload.channel_id)
         user = client.get_user(payload.user_id)
         message = await channel.fetch_message(payload.message_id)
         emoji = str(payload.emoji)
-        return
+
 
     async def on_raw_reaction_remove(self, payload):
         return
 
+
     async def on_member_join(self, member):
         pass
+
+
     async def on_member_remove(self, member):
         pass
+
+
     async def on_member_update(self, before, after):
         pass
+
 
 with open("token", "r") as tkn:
     token = str(tkn.read())
